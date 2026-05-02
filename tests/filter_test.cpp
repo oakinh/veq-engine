0ee72135dec94b7d9b6439ae4943f22066a5ba52
oakinh <nikaohill@gmail.com>
2026-05-02 07:18:07 -0500

    Add more filter and scan tests

--- tests/filter_test.cpp
+++ tests/filter_test.cpp
@@ -2,16 +2,20 @@
 #include <veq/execution/scan.hpp>
 #include <gtest/gtest.h>
 #include <algorithm>
+#include <ranges>
+
 #include "fixtures/table_fixtures.hpp"
 
 class FilterTest : public testing::Test {
 protected:
     veq::Table t0_ { veq::test::makeTinyTable() };
     veq::Scan s0_ { t0_, 4 };
+    veq::Scan s1_ { t0_, 5 };
+    veq::Scan s2_ { t0_, 1 };
     veq::Filter f0_ {};
 };
 
-TEST_F(FilterTest, FiltersDataAsExpected) {
+TEST_F(FilterTest, FiltersDataWithNonContiguousSelection) {
      const veq::FilterOperation<std::greater<>> filter_op { std::greater<>{}, 30 };
 
     { // Batch 0
@@ -32,4 +36,25 @@ TEST_F(FilterTest, FiltersDataAsExpected) {
         EXPECT_EQ(batch2.selection.size(), 0);
     }
     EXPECT_FALSE(s0_.hasNextBatch());
+}
+
+TEST_F(FilterTest, ReturnsFullSelectionWhenAllRowsMatchWithBatchSize5) {
+    // TODO: Infinite loop
+    const veq::FilterOperation<std::greater<>> filter_op { std::greater<>{}, 0 };
+    int i {};
+    for (; s1_.hasNextBatch(); ++i) {
+        veq::SelectedBatch batch { f0_.apply(s1_.nextBatch(), veq::ColumnView{ t0_.age.data() }, filter_op) };
+        EXPECT_EQ(batch.size, 5);
+    }
+    EXPECT_EQ(i, 2);
+}
+
+TEST_F(FilterTest, ReturnsFullSelectionWhenAllRowsMatchWithBatchSize1) {
+    const veq::FilterOperation<std::greater<>> filter_op { std::greater<>{}, 0 };
+    int i {};
+    for (; s2_.hasNextBatch(); ++i) {
+        veq::SelectedBatch batch { f0_.apply(s2_.nextBatch(), veq::ColumnView{ t0_.age.data() }, filter_op) };
+        EXPECT_EQ(batch.size, 1);
+    }
+    EXPECT_EQ(i, 10);
 }
