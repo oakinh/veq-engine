80361c1b0313c8051f231f23bc0968df9adae0f6
oakinh <nikaohill@gmail.com>
2026-05-07 17:55:57 -0500

    Add projection tests

---
+++ tests/pipeline_test.cpp
@@ -0,0 +1,3 @@
+//
+// Created by nikao on 5/7/2026.
+//
--- tests/projection_test.cpp
+++ tests/projection_test.cpp
@@ -22,7 +22,7 @@ protected:
     veq::Projection p0_ {};
 };
 
-TEST_F(ProjectionTest, ProjectionPerformsZeroCopies) {
+TEST_F(ProjectionTest, PerformsZeroCopies) {
     p0_.setTargetColumns({ veq::ColumnView{ t0_.id.data() }});
     veq::ProjectedBatch projected_batch { p0_.apply(sb0_) };
 
@@ -37,4 +37,30 @@ TEST_F(ProjectionTest, ProjectionReturnsOnlySelectedColumns) {
 
     EXPECT_EQ(columns.size(), 1);
     EXPECT_EQ(columns[0].data, t0_.id.data());
+}
+
+TEST_F(ProjectionTest, ReturnsMultipleSelectedColumns) {
+    p0_.setTargetColumns({
+        veq::ColumnView{ t0_.id.data() },
+        veq::ColumnView{ t0_.age.data() },
+        veq::ColumnView{ t0_.occupation_id.data() }
+    });
+
+    auto [ columns, selection, selection_size ] { p0_.apply(sb0_) };
+    EXPECT_EQ(columns.size(), 3);
+}
+
+TEST_F(ProjectionTest, PreservesTargetColumnOrder) {
+    const veq::ColumnViews target_columns {
+        veq::ColumnView{ t0_.age.data() },
+        veq::ColumnView{ t0_.id.data() },
+        veq::ColumnView{ t0_.occupation_id.data() }
+    };
+
+    p0_.setTargetColumns(target_columns);
+
+    auto [ columns, selection, selection_size ] { p0_.apply(sb0_) };
+    for (std::size_t i {}; i < target_columns.size(); ++i) {
+        ASSERT_EQ(target_columns[i].data, columns[i].data);
+    }
 }
