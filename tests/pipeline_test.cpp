f0caac4144bb92378e26de12d96edacdd959eab0
oakinh <nikaohill@gmail.com>
2026-05-08 08:57:06 -0500

    Write runPipeline helper and a TestMaterializer

--- tests/pipeline_test.cpp
+++ tests/pipeline_test.cpp
@@ -4,6 +4,7 @@
 #include <veq/execution/scan.hpp>
 #include <veq/execution/projection.hpp>
 #include "fixtures/table_fixtures.hpp"
+#include "util/run_pipeline.hpp"
 
 class PipelineTest : public testing::Test {
 protected:
@@ -25,12 +26,14 @@ TEST_F(PipelineTest, PerformsNoCopies) {
             const std::span<const veq::ColumnView> source_columns,
             const std::span<const veq::ColumnView> result_columns
         ) {
-        ASSERT_GT(source_columns.size(), 0);
-        for (std::size_t i {}; i < source_columns.size(); ++i) {
-            ASSERT_EQ(source_columns[i].data, result_columns[i].data);
+            ASSERT_GT(source_columns.size(), 0);
+            ASSERT_GT(result_columns.size(), 0);
+            for (std::size_t i {}; i < source_columns.size() && i < result_columns.size(); ++i) {
+                ASSERT_EQ(source_columns[i].data, result_columns[i].data);
         }
     };
 
+    std::size_t i {};
     while (s0_.hasNextBatch()) {
         const veq::ColumnBatch batch { s0_.nextBatch() };
         verifyPointerEquality(column_views_, batch.columns);
@@ -39,6 +42,29 @@ TEST_F(PipelineTest, PerformsNoCopies) {
         verifyPointerEquality(column_views_, selected_batch.columns);
         const veq::ProjectedBatch projected_batch { p0_.apply(selected_batch)};
         verifyPointerEquality(column_views_, projected_batch.columns);
-
+        ++i;
     }
+    ASSERT_GT(i, 0);
+}
+
+TEST_F(PipelineTest, DeliversExpectedResultFromSmallBatchAndTinyTable) {
+    // std::size_t i {};
+    // while (s0_.hasNextBatch()) {
+    //     const veq::ColumnBatch batch { s0_.nextBatch() };
+    //
+    //     const veq::SelectedBatch selected_batch { f0_.apply(batch, filter_target_column0_, filter_op0_) };
+    //     p0_.setTargetColumns(column_views_);
+    //     const veq::ProjectedBatch projected_batch { p0_.apply(selected_batch)};
+    //
+    //     ++i;
+    // }
+    // ASSERT_GT(i, 0);
+    veq::test::PipelineRunOutput output { veq::test::runPipeline<std::greater<>>({
+        .scan=s0_,
+        .filter=f0_,
+        .projection=p0_,
+        .projection_columns=column_views_,
+        .filter_target_column=filter_target_column0_,
+        .filter_op = filter_op0_
+        }) };
 }
---
+++ tests/util/test_materializer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <veq/execution/projection.hpp>
+
+namespace veq::test {
+    class TestMaterializer {
+    public:
+        void consume(const ProjectedBatch& batch) {
+            for (std::size_t i {}; i < batch.columns.size(); ++i) {
+                if (i == columns_.size()) { columns_.push_back({}); }
+                for (std::size_t j {}; j < batch.selection_size; ++j) {
+                    std::size_t selection_idx { batch.selection[j] };
+                    columns_[i].push_back(batch.columns[i].data[selection_idx]);
+                }
+            }
+        }
+
+        const std::vector<std::vector<uint64_t>>& columns() const { return columns_; }
+        void reset() { *this = {}; }
+
+    private:
+        std::vector<std::vector<uint64_t>> columns_ {};
+    };
+
+}
