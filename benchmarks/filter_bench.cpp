154a5fd34ce6a3b78996a167089f3108785a9ac4
oakinh <nikaohill@gmail.com>
2026-05-14 08:39:31 -0500

    Begin writing filter_bench and column_batch_builders

--- benchmarks/filter_bench.cpp
+++ benchmarks/filter_bench.cpp
@@ -1,3 +1,24 @@
-//
-// Created by oakin on 5/14/26.
-//
+#include <benchmark/benchmark.h>
+#include <cstddef>
+#include <veq/execution/filter.hpp>
+#include "util/column_batch_builders.hpp"
+
+namespace veq::bench {
+    static void BM_FilterAge(benchmark::State& state) {
+        auto row_count { state.range(0) };
+        Table table { buildRandomAgeTable(row_count) };
+        std::vector<ColumnBatch> batches { buildBatches(table) };
+
+        Filter filter {};
+        FilterOperation<std::greater<>> filter_op { std::greater{}, 50 };
+
+        for (auto _ : state) {
+
+            for (const auto& batch : batches) {
+                filter.apply(batch, ColumnView{ table.age.data() }, filter_op);
+            }
+
+        }
+    }
+}
+
