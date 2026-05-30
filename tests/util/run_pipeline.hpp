1ebf746e19e52364a3cabfe09af0651491268a72
oakinh <nikaohill@gmail.com>
2026-05-30 14:39:43 -0500

    Prepare runPipeline utils for adding CountAggregation

--- tests/util/run_pipeline.hpp
+++ tests/util/run_pipeline.hpp
@@ -8,8 +8,22 @@
 #include <veq/storage/column.hpp>
 
 namespace veq::test {
+// TODO: Later these inputs need to be thought through more...
+// We're sort of copying a ton of references to create a struct
+// Seems quite wonky. It could be that my TS experience is giving me the wrong instincts here
+
+
+template <typename Compare>
+struct ScanFilterProjectRunInput {
+    Scan& scan;
+    Filter& filter;
+    Projection& projection;
+    const ColumnView& filter_target_column;
+    const FilterOperation<Compare>& filter_op;
+};
+
 template<typename Compare>
-struct PipelineRunInput {
+struct RunPipelineAndMaterializeInput {
     Scan& scan;
     Filter& filter;
     Projection& projection;
@@ -23,19 +37,33 @@ struct PipelineRunOutput {
     std::size_t batches_run{};
 };
 
+template <typename Compare>
+ProjectedBatch scanFilterProject(ScanFilterProjectRunInput<Compare> inputs) {
+    auto& [scan, filter, projection, filter_target_column, filter_op] = inputs;
+
+    const ColumnBatch batch{ scan.nextBatch() };
+    const SelectedBatch selected_batch{ filter.apply(batch, filter_target_column, filter_op) };
+
+    return { projection.apply(selected_batch) };
+}
+
 template<typename Compare>
-PipelineRunOutput runPipeline(PipelineRunInput<Compare> inputs) {
-    auto [scan, filter, projection, projection_columns, filter_target_column, filter_op,
+PipelineRunOutput runPipeline(RunPipelineAndMaterializeInput<Compare> inputs) {
+    auto& [scan, filter, projection, projection_columns, filter_target_column, filter_op,
           materializer_out] = inputs;
+
+    projection.setTargetColumns(projection_columns);
 
     std::size_t i{};
     while (scan.hasNextBatch()) {
-        const ColumnBatch batch{ scan.nextBatch() };
+        ProjectedBatch projected_batch { scanFilterProject<std::greater<>>({
+            scan,
+            filter,
+            projection,
+            filter_target_column,
+            filter_op
+            })};
 
-        projection.setTargetColumns(projection_columns);
-        const SelectedBatch selected_batch{ filter.apply(batch, filter_target_column, filter_op) };
-
-        const ProjectedBatch projected_batch{ projection.apply(selected_batch) };
         materializer_out.consume(projected_batch);
         ++i;
     }
