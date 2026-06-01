2e6f66b29f2bd876a79f1529a688b53da688bd9d
oakinh <nikaohill@gmail.com>
2026-06-01 10:58:41 -0500

    Fix implicit conversion warnings

--- shared/util.hpp
+++ shared/util.hpp
@@ -2,11 +2,11 @@
 #include <random>
 
 namespace veq::shared {
-    inline std::uint64_t generateRandomNumber(std::size_t seed, std::size_t min, std::size_t max) {
+    inline std::uint64_t generateRandomNumber(std::size_t seed, int min, int max) {
         std::mt19937 gen(seed);
 
-        std::uniform_int_distribution<int> dist (min, max);
+        std::uniform_int_distribution<int> dist(min, max);
 
-        return dist(gen);
+        return static_cast<std::uint64_t>(dist(gen));
     }
 }
